69fa8b0328d6b7e43814dbaea7e753a069d2c2d1
Atuesta <jatuesta@illumina.com>
2022-07-01 16:39:34 -0700

    learning for loops in python

--- learning_python_files/learning_for_loop_in_python.py
+++ learning_python_files/learning_for_loop_in_python.py
@@ -1,1 +1,8 @@
 #Learning for loops in python
+#used to iterate over the sequence like list, string, dic, set or tuple
+#Less like the for loop in other programming languanges
+
+city = "Delhi"
+
+for c in city:
+    print(c)
