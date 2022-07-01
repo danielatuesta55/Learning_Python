c708032f57d7d7256dee65a04f8a0cb983961a90
Atuesta <jatuesta@illumina.com>
2022-07-01 08:55:22 -0700

    Learning Dictionaries

---
+++ learning_python_files/learning_dictionarys_in_python.py
@@ -0,0 +1,29 @@
+""" Dictionaries are used to store data in key:value pairs. The are changable (mutable)
+Defining Dictionary
+Accesing items from dictionary
+Adding items to dictionary
+Removing items from dictionary"""
+
+demo_dict0 ={}
+demo_dict = {1:20, 2:45, 3:50,6:67}
+demo_dict1 = {"wa":"testurl","uat":"utaurl"}
+demo_dict2 = {"wa":23, 3:"utaurl"}
+
+print(demo_dict0)
+print(demo_dict)
+print(demo_dict1)
+print(demo_dict2)
+
+#you can get the values associated with the kyes in the dictionary. For example you want to get the name of the city 
+
+demo_dict3 = {"city":"Bogota", "country":"Colombia", "population":48000000}
+
+print(demo_dict3["city"]) #It will print Bogota
+
+#To add values to a dictionary you call the dictionary than['key']='value
+demo_dict3["colors_of_the_flag"]= "Yellow, Blue, Red"
+print(demo_dict3)
+
+#To delete a value from a dictionary you can use the .pop('key') 
+demo_dict3.pop('colors_of_the_flag')
+print(demo_dict3)
