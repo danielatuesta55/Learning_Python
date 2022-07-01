ef0f3357581c9c677d7a14d459532900334a6c4f
Atuesta <jatuesta@illumina.com>
2022-07-01 16:48:12 -0700

    learning for loops in python

--- learning_python_files/learning_for_loop_in_python.py
+++ learning_python_files/learning_for_loop_in_python.py
@@ -2,7 +2,21 @@
 #used to iterate over the sequence like list, string, dic, set or tuple
 #Less like the for loop in other programming languanges
 
-city = "Delhi"
+city = ["Delhi","Mealbourne","Lucknow","Sydney"] #same for string tuples adn sets
 
 for c in city:
     print(c)
+
+
+cities = [["India","Delhi"],["Sydney","Mealbourne"],["Canada","Lucknow"],["USA","New York"]] 
+
+for country, city in cities:
+    print("country is "+ country,"and city is "+ city)
+
+#For dictionarys
+#function to convert into dictionary 
+my_dic = dict(cities)
+print(my_dic)
+#for loop in dic 
+for country, city in my_dic.items(): #Make sure you use the .item() method for dictionary or else it will not allow you to loop through
+    print(country, city)
