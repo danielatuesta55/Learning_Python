e39b6eb252783cdb87d1e3d666634c7a16ede069
Atuesta <jatuesta@illumina.com>
2022-07-01 09:05:12 -0700

    Learning Dictionaries

--- learning_python_files/learning_dictionarys_in_python.py
+++ learning_python_files/learning_dictionarys_in_python.py
@@ -27,3 +27,25 @@ print(demo_dict3)
 #To delete a value from a dictionary you can use the .pop('key') 
 demo_dict3.pop('colors_of_the_flag')
 print(demo_dict3)
+
+"""Dictionary Methods
+get() - retunrs the value for specified key in the dictionary
+keys() Returns a copy of the dictionary key's
+items() Returns a copy for the dictionary key value pair
+values() Returns a copy of the values in the dictionary
+pop() removes the dictionary with specifed key
+popitem() Removes the arbitrary key:value pair
+update() Adds the specifed key-value pair to dictionary
+copy() Returns a copy of the dictionary
+clear() Removes all the elements from the dictionary"""
+
+demo_dict4 = {"city":"Bogota", "country":"Colombia", "population":48000000}
+print(demo_dict4.get("city"))
+print(demo_dict4.keys())
+print(demo_dict4.items())
+print(demo_dict4.values())
+print(demo_dict4.pop("population"))
+print(demo_dict4.popitem())#removes the last item of the dictionary
+print(demo_dict4.update({"flag_color":"Yellow, Blue, Red"}))
+print(demo_dict4.copy())
+print(demo_dict4.clear())
