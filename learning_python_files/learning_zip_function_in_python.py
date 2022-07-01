f012f2b832a87230b3dc67b0701d1c247d186f93
Atuesta <jatuesta@illumina.com>
2022-07-01 16:37:37 -0700

    learning zip function in python

--- learning_python_files/learning_zip_function_in_python.py
+++ learning_python_files/learning_zip_function_in_python.py
@@ -1,1 +1,17 @@
 #learning zip function in python 
+
+list1 = ['India', 'USA', 'Australia', 'UK']
+list2 = ['Pune', "New York", "Sydeny", "London"]
+
+s = zip(list1,list2)
+print(list(s))
+
+for x,y in zip(list1,list2):
+    print(x,y)
+
+#real life examplee
+total_cost = [45,67,65,87]
+sales_price = [55,77,89,76]
+
+for x,y in zip(total_cost,sales_price):
+    print(y-x)
