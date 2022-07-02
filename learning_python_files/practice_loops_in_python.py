75259547f361c0edae904a84677e15c0968ceb5a
Atuesta <jatuesta@illumina.com>
2022-07-02 16:33:38 -0700

    practice loops in python doc

--- learning_python_files/practice_loops_in_python.py
+++ learning_python_files/practice_loops_in_python.py
@@ -1,1 +1,23 @@
 #This file has been made so I can practice my loops and methods. I was sitting in by the pool and had to come up and try some of this out
+#Started by creating all types of objects. You can double check with the printed messade of type()
+string1 = "Jorge"
+print(string1.type())
+string2  = "Daniel"
+print(string2.type())
+list1 = ['iPad','iPhone','macbook','screen','bottel cap','paper','keyboard', 'mouse']
+print(list1.type())
+list2 = ['couch','tv','remote control', 'play station', 'chair', 'blanket', 'table', 'mug']
+print(list2.type())
+tuple1 = (1,2,3,4,5,6,7,8,9)
+print(tuple1.type())
+tuple2 = (83.0,82.5,81.5,81.5,82.8)
+print(tuple2.type())
+dict1 = {"country":"Colombia","city":"Bogota","country":"USA","city":"Chicago","country":"USA","Spain":"Madrid"}
+print(dict1.type())
+dict2 = {"male":80,'female':68,"male":86,'female':88,"male":100,'female':75}
+print(dict2.type())
+set1 = {'cool','bannanas', 'life','podcast','enjoy','coding'}
+print(set1.type())
+set2 = {85,63.2,65,98,84,110,5,55,5.5}
+print(set2.type())
+
